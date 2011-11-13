b8a93d86f21a0ca3afcc65ae32a0d1cb3e2d5e8d
Ross Beehler <ross@sipp.net>
2011-11-12 23:40:16 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yahtzee::Application.config.secret_token = 'adca0021e5a6cdfeec37a573fd29981f3ea059ffd2992a1d2d29d03be085311cfc38e63725022bd40e41e3cf2a07c28def2ca6a3cc0df7d7ae9cdb76aacf76d6'
